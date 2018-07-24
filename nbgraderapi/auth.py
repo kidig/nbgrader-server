09a70d2ff4056ea204961abfbe1bf9de4e78002e
Dmitrii Gerasimenko <kiddima@gmail.com>
2018-07-24 09:50:32 +0200

    fix auth without a token

--- nbgraderapi/auth.py
+++ nbgraderapi/auth.py
@@ -6,15 +6,16 @@ def token_required(method):
 
     @functools.wraps(method)
     def wrapper(self, *args, **kwargs):
-        token = self.get_token_from_authorization_header()
-
-        if not token:
-            raise tornado.web.HTTPError(401, 'Unauthorized Access. Token is missing.')
-
         auth_token = self.application.settings.get('auth_token')
 
-        if auth_token and not token == auth_token:
-            raise tornado.web.HTTPError(403, 'Auth token invalid.')
+        if auth_token:
+            token = self.get_token_from_authorization_header()
+
+            if not token:
+                raise tornado.web.HTTPError(401, 'Unauthorized access. Token is missing.')
+
+            if not token == auth_token:
+                raise tornado.web.HTTPError(403, 'Invalid token.')
 
         return method(self, *args, **kwargs)
     return wrapper
